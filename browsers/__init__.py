8cf6965beb0908a56681ce9f050a072183386c88
Ronie Martinez <ronmarti18@gmail.com>
2022-04-12 23:37:32 +0200

    ✨ Add support for Linux using *.desktop entries

--- browsers/__init__.py
+++ browsers/__init__.py
@@ -8,7 +8,7 @@ from typing import Dict, Iterator, Optional, Sequence, Tuple
 logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 logger = logging.getLogger(__name__)
 
-BROWSER_LIST = (
+OSX_BROWSER_BUNDLE_LIST = (
     # browser name, bundle ID, version string
     ("chrome", "com.google.Chrome", "KSVersion"),
     ("chrome-canary", "com.google.Chrome.canary", "KSVersion"),
@@ -30,9 +30,49 @@ BROWSER_LIST = (
     ("brave-nightly", "com.brave.Browser.nightly", "CFBundleVersion"),
 )
 
+LINUX_DESKTOP_ENTRY_LIST = (
+    ("chrome", ("google-chrome",)),
+    ("chromium", ("chromium",)),
+    ("firefox", ("firefox", "firefox_firefox")),
+    ("msedge", ("microsoft-edge",)),
+    ("opera", ("opera_opera",)),
+    ("opera-beta", ("opera-beta_opera-beta",)),
+    ("opera-developer", ("opera-developer_opera-developer",)),
+    ("brave", ("brave-browser", "brave_brave")),
+    ("brave-beta", ("brave-browser-beta",)),
+    ("brave-nightly", ("brave-browser-nightly",)),
+)
+
+# $XDG_DATA_HOME and $XDG_DATA_DIRS are not always set
+XDG_DATA_LOCATIONS = (
+    "~/.local/share/applications",
+    "/usr/share/applications",
+    "/var/lib/snapd/desktop/applications",
+)
+
 
 def get_available_browsers() -> Iterator[Tuple[str, Dict]]:
     platform = sys.platform
+    if platform == "linux":
+        from xdg.DesktopEntry import DesktopEntry
+
+        for browser, desktop_entries in LINUX_DESKTOP_ENTRY_LIST:
+            for application_dir in XDG_DATA_LOCATIONS:
+                # desktop entry name can be "firefox.desktop" or "firefox_firefox.desktop"
+                for desktop_entry in desktop_entries:
+                    path = os.path.join(application_dir, f"{desktop_entry}.desktop")
+                    if not os.path.isfile(path):
+                        continue
+                    entry = DesktopEntry(path)
+                    executable_path = entry.getExec()
+                    if executable_path.lower().endswith(" %u"):
+                        executable_path = executable_path[:-3].strip()
+                    # FIXME: --version includes the name for most browsers
+                    version = subprocess.getoutput(f"{executable_path} --version")
+                    info = dict(path=executable_path, display_name=entry.getName(), version=version)
+                    print(info)
+                    yield browser, info
+        return
     if platform != "darwin":  # pragma: no cover
         logger.info(
             "'%s' is currently not supported. Please open an issue or a PR at '%s'",
@@ -40,7 +80,7 @@ def get_available_browsers() -> Iterator[Tuple[str, Dict]]:
             "https://github.com/roniemartinez/browsers",
         )
         return
-    for browser, bundle_id, version_string in BROWSER_LIST:
+    for browser, bundle_id, version_string in OSX_BROWSER_BUNDLE_LIST:
         paths = subprocess.getoutput(f'mdfind "kMDItemCFBundleIdentifier == {bundle_id}"').splitlines()
         for path in paths:
             with open(os.path.join(path, "Contents/Info.plist"), "rb") as f:
@@ -65,4 +105,7 @@ def launch(browser: str, url: str, args: Optional[Sequence[str]] = None) -> None
 
 
 def _launch(path: str, url: str, args: Sequence[str]) -> None:  # pragma: no cover
-    subprocess.Popen(["open", "--wait-apps", "--new", "--fresh", "-a", path, url, "--args", *args])
+    command = [path, url, "--args", *args]
+    if sys.platform == "darwin":
+        command = ["open", "--wait-apps", "--new", "--fresh", "-a", *command]
+    subprocess.Popen(command)
