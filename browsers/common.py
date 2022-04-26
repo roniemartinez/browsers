cff7abb419b83b8ce43572ba8ebb0f6af3eb4494
Ronie Martinez <ronmarti18@gmail.com>
2022-04-26 22:39:51 +0200

    Use typing_extensions on 3.7

--- browsers/common.py
+++ browsers/common.py
@@ -1,4 +1,9 @@
-from typing import TypedDict
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
 
 
 class Browser(TypedDict):
