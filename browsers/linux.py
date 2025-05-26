bb66d9091362c5475da3488c7ac9800377523a88
Ronie Martinez <ronmarti18@gmail.com>
2025-05-26 12:43:32 +0100

    🐧 Rewrite Linux desktop file search

--- browsers/linux.py
+++ browsers/linux.py
@@ -1,4 +1,5 @@
 import configparser
+import glob
 import os
 import re
 import shlex
@@ -9,22 +10,21 @@ from typing import Iterator
 
 from .common import Browser
 
-LINUX_DESKTOP_ENTRY_LIST = (
-    # desktop entry name can be "firefox.desktop" or "firefox_firefox.desktop"
-    ("brave", ("brave-browser", "brave_brave")),
-    ("brave-beta", ("brave-browser-beta",)),
-    ("brave-nightly", ("brave-browser-nightly",)),
-    ("chrome", ("google-chrome",)),
-    ("chromium", ("chromium", "chromium_chromium")),
-    ("falkon", ("falkon_falkon",)),
-    ("firefox", ("firefox", "firefox_firefox")),
-    ("konqueror", ("konqueror_konqueror",)),
-    ("msedge", ("microsoft-edge",)),
-    ("opera", ("opera_opera",)),
-    ("opera-beta", ("opera-beta_opera-beta",)),
-    ("opera-developer", ("opera-developer_opera-developer",)),
-    ("vivaldi", ("vivaldi_vivaldi-stable",)),
-)
+LINUX_DESKTOP_BROWSER_NAMES = {
+    "Brave Web Browser": "brave",
+    "Brave Web Browser (beta)": "brave-beta",
+    "Brave Web Browser (nightly)": "brave-nightly",
+    "Google Chrome": "chrome",
+    "Chromium Web Browser": "chromium",
+    "Falkon": "falkon",
+    "Firefox": "firefox",
+    "Konqueror": "konqueror",
+    "Microsoft Edge": "msedge",
+    "Opera": "opera",
+    "Opera beta": "opera-beta",
+    "Opera developer": "opera-developer",
+    "Vivaldi": "vivaldi",
+}
 
 # $XDG_DATA_HOME and $XDG_DATA_DIRS are not always set
 XDG_DATA_LOCATIONS = (
@@ -38,37 +38,40 @@ VERSION_PATTERN = re.compile(r"\b(\S+\.\S+)\b")  # simple pattern assuming all v
 
 def browsers() -> Iterator[Browser]:  # type: ignore[return]
     if sys.platform == "linux":
-        for browser, desktop_entries in LINUX_DESKTOP_ENTRY_LIST:
-            for application_dir in XDG_DATA_LOCATIONS:
-                for desktop_entry in desktop_entries:
-                    path = os.path.join(application_dir, f"{desktop_entry}.desktop")
+        for application_dir in XDG_DATA_LOCATIONS:
+            for file_name in glob.glob("*.desktop", root_dir=application_dir):
+                desktop_file = os.path.join(application_dir, file_name)
+                config = configparser.ConfigParser(interpolation=None)
+                config.read(desktop_file, encoding="utf-8")
+                if "WebBrowser" not in config.get("Desktop Entry", "Categories").split(";") and config.get(
+                        "Desktop Entry", "GenericName") != "Web Browser":
+                    continue
 
-                    if not os.path.isfile(path):
+                display_name = config.get("Desktop Entry", "Name")
+                executable_path = config.get(
+                    "Desktop Entry", "TryExec", fallback=config.get("Desktop Entry", "Exec")
+                )
+
+                # Try to remove BAMF_DESKTOP_FILE_HINT and find the actual executable/binary
+                for path in shlex.split(executable_path):
+                    if path == "env":
                         continue
+                    if os.path.exists(path):
+                        executable_path = path
+                        break
+                    # Find binary path from $PATH
+                    # see https://specifications.freedesktop.org/desktop-entry-spec/latest/exec-variables.html
+                    if result := shutil.which(path):
+                        executable_path = result
+                        break
 
-                    config = configparser.ConfigParser(interpolation=None)
-                    config.read(path, encoding="utf-8")
-                    name = config.get("Desktop Entry", "Name")
+                version = subprocess.getoutput(f"{executable_path} --version 2>&1").strip()
+                if match := VERSION_PATTERN.search(version):
+                    version = match[0]
 
-                    executable_path = config.get(
-                        "Desktop Entry", "TryExec", fallback=config.get("Desktop Entry", "Exec")
-                    )
-
-                    # Try to remove BAMF_DESKTOP_FILE_HINT and find the actual executable/binary
-                    for path in shlex.split(executable_path):
-                        if path == "env":
-                            continue
-                        if os.path.exists(path):
-                            executable_path = path
-                            break
-                        # Find binary path from $PATH
-                        # see https://specifications.freedesktop.org/desktop-entry-spec/latest/exec-variables.html
-                        if result := shutil.which(path):
-                            executable_path = result
-                            break
-
-                    version = subprocess.getoutput(f"{executable_path} --version 2>&1").strip()
-                    if match := VERSION_PATTERN.search(version):
-                        version = match[0]
-
-                    yield Browser(browser_type=browser, path=executable_path, display_name=name, version=version)
+                yield Browser(
+                    browser_type=LINUX_DESKTOP_BROWSER_NAMES.get(display_name, "unknown"),
+                    path=executable_path,
+                    display_name=display_name,
+                    version=version,
+                )
