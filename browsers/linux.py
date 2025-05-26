f3be76ab9aca3f07001ccc900e997251add7926f
Ronie Martinez <ronmarti18@gmail.com>
2025-05-26 17:14:49 +0100

    ✨ Support Flatpak

--- browsers/linux.py
+++ browsers/linux.py
@@ -11,10 +11,14 @@ from typing import Iterator
 from .common import Browser
 
 LINUX_DESKTOP_BROWSER_NAMES = {
+    "Brave": "brave",
     "Brave Web Browser": "brave",
     "Brave Web Browser (beta)": "brave-beta",
     "Brave Web Browser (nightly)": "brave-nightly",
+    "Floorp": "floorp",
     "Google Chrome": "chrome",
+    "Google Chrome dev": "chrome-dev",
+    "Chromium": "chromium",
     "Chromium Web Browser": "chromium",
     "Falkon": "falkon",
     "Firefox": "firefox",
@@ -24,14 +28,19 @@ LINUX_DESKTOP_BROWSER_NAMES = {
     "Opera": "opera",
     "Opera beta": "opera-beta",
     "Opera developer": "opera-developer",
+    "Ungoogled Chromium": "ungoogled-chromium",
     "Vivaldi": "vivaldi",
+    "Zen Browser": "zen",
 }
 
-# $XDG_DATA_HOME and $XDG_DATA_DIRS are not always set
 XDG_DATA_LOCATIONS = (
+    # $XDG_DATA_HOME and $XDG_DATA_DIRS are not always set
     "~/.local/share/applications",
     "/usr/share/applications",
     "/var/lib/snapd/desktop/applications",
+    # Flatpak
+    "~/.local/share/flatpak/exports/share/applications",
+    "/var/lib/flatpak/exports/share/applications",
 )
 
 VERSION_PATTERN = re.compile(r"\b(\S+\.\S+)\b")  # simple pattern assuming all version strings have a dot on them
@@ -40,7 +49,7 @@ VERSION_PATTERN = re.compile(r"\b(\S+\.\S+)\b")  # simple pattern assuming all v
 def browsers() -> Iterator[Browser]:  # type: ignore[return]
     if sys.platform == "linux":
         for application_dir in XDG_DATA_LOCATIONS:
-            for desktop_file in glob.glob(os.path.join(application_dir, "*.desktop")):
+            for desktop_file in glob.glob(os.path.join(os.path.expanduser(application_dir), "*.desktop")):
                 config = configparser.ConfigParser(interpolation=None)
                 config.read(desktop_file, encoding="utf-8")
                 if (
@@ -50,7 +59,12 @@ def browsers() -> Iterator[Browser]:  # type: ignore[return]
                     continue
 
                 display_name = config.get("Desktop Entry", "Name")
-                executable_path = config.get("Desktop Entry", "TryExec", fallback=config.get("Desktop Entry", "Exec"))
+                if flatpak_name := config.get("Desktop Entry", "X-Flatpak", fallback=""):
+                    executable_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(desktop_file))), "bin", flatpak_name)
+                else:
+                    executable_path = config.get(
+                        "Desktop Entry", "TryExec", fallback=config.get("Desktop Entry", "Exec")
+                    )
 
                 # Try to remove BAMF_DESKTOP_FILE_HINT and find the actual executable/binary
                 for path in shlex.split(executable_path):
